4ce04ab2724f15eb598bf7522ac9339aa9b91b82
Yuval Zach <zachyuval@gmail.com>
2018-10-01 22:48:07 -0700

    Unify code style and resolve numerous compile error in sorting

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -13,34 +13,39 @@ void swap(int *x, int *y)
 }
 
 // Implement bubble sort
-void bubbleSort(int arr[], int n)
+void bubble_sort(int arr[], size_t n)
 {
-   int i, j;
-   for (i = 0; i < n-1; i++)
-	 	// last i elements are already in place
-		for (j = 0; j < n-i-1; j++)
-			if (arr[j] > arr[j+1])
-				swap(&arr[j], &arr[j+1]);
+   for (size_t i = 0; i < n - 1; i++)
+   {
+	   	// last i elements are already in place
+		for (size_t j = 0; j < n-i-1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(&arr[j], &arr[j + 1]);
+			}
+		}
+   }
 }
 
-// Function to print elements
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i=0; i < size; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
+// A utility function to print an array of size n 
+void print_array(int arr[], int n) 
+{ 
+   for (size_t i = 0; i < n; i++)
+    {
+       std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl; 
+} 
 
-// test
 int main()
 {
 	int arr[] = {46, 24, 33, 10, 2, 81, 50};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	printf("Unsorted array: \n");
-	printArray(arr, n);
-	bubbleSort(arr, n);
-  printf("Sorted array: \n");
-	printArray(arr, n);
-  return 0;
+	std::cout << "Unsorted array:" << std::endl;
+	print_array(arr, n);
+	bubble_sort(arr, n);
+  	std::cout << "Sorted array:" << std::endl;
+	print_array(arr, n);
+  	return 0;
 }
