37d83faf6ebfd4a3c52382dbf4e89479570fc257
suraj-goel <surajgoel.1225@gmail.com>
2018-10-02 08:40:29 +0530

    Added Dp Programs

---
+++ Dyanamic_Programming/waysToCover.cpp
@@ -0,0 +1,28 @@
+
+// A Dynamic Programming based C++ program to count number of ways 
+// to cover a distance with 1, 2 and 3 steps 
+#include<iostream> 
+using namespace std; 
+  
+int printCountDP(int dist) 
+{ 
+    int count[dist+1]; 
+  
+    // Initialize base values. There is one way to cover 0 and 1 
+    // distances and two ways to cover 2 distance 
+    count[0]  = 1,  count[1] = 1,  count[2] = 2; 
+  
+    // Fill the count array in bottom up manner 
+    for (int i=3; i<=dist; i++) 
+       count[i] = count[i-1] + count[i-2] + count[i-3]; 
+  
+    return count[dist]; 
+} 
+  
+// driver program 
+int main() 
+{ 
+    int dist = 4; 
+    cout << printCountDP(dist); 
+    return 0; 
+} 
