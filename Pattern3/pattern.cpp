3cf2e525a5fe54faa4802517327ab76d9a7faabc
dishantsethi <dishantsethi14@gmail.com>
2018-10-03 18:54:31 +0530

    pattern

---
+++ Pattern3/pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char input, alphabet = 'A';
+
+    cout << "Enter the uppercase character you want to print in the last row: ";
+    cin >> input;
+
+    for(int i = 1; i <= (input-'A'+1); ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << alphabet << " ";
+        }
+        ++alphabet;
+
+        cout << endl;
+    }
+    return 0;
+}
