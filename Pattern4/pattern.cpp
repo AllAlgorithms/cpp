5956048cf8981fd6b172c1cc6757ffee5fe22394
dishantsethi <dishantsethi14@gmail.com>
2018-10-03 18:57:11 +0530

    pattern

---
+++ Pattern4/pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
