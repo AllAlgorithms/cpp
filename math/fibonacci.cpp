810b933178242431982d21430690633fdd53744c
anupamdas104 <anupamdas104@gmail.com>
2018-10-04 14:03:33 -0400

    added docmuentation for the fibonacci function

--- math/fibonacci.cpp
+++ math/fibonacci.cpp
@@ -4,11 +4,16 @@
 using std::cin;
 using std::cout;
 
+//returns fibonacci number for specified position
 int64_t getFibonnaci(unsigned int input){
+	//base case
 	if (input <= 1){
 		return input;
 	}
 	
+	//recursively calls getFibonnaci function to get the previous fibonacci numbers until base case is reached
+	//all the previous fibonacci numbers are then added
+	//and returns the fibonacci number for current position
 	return getFibonnaci(input-1) + getFibonnaci(input-2);
 }
 
@@ -16,6 +21,7 @@ int64_t getFibonnaci(unsigned int input){
 int main(){
 	int userInput;
 	
+	//gets user Input
 	cout << "Enter a number : \n";
 	cin >> userInput;
 	
