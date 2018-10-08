016c37549583999f722e90929abe645d7cb03ad1
Manisha Keim <manisha.keim@gmail.com>
2018-10-08 21:03:01 +0530

    Added binary_search using file input in c++

---
+++ searches/bin.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<math.h>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<algorithm>
+
+using namespace std;
+
+int counter = 0;
+
+int binarySearch(vector<int> &arr, unsigned int low, unsigned int high, int num){
+
+	if(low > high)
+		return -1;
+	else
+	{
+		int mid = (low + high) / 2;
+		counter++;
+
+		if(arr[mid] == num)
+			return mid;
+		else if(arr[mid] < num)
+				return binarySearch(arr, mid + 1, high, num);
+		else
+				return binarySearch(arr, low, mid-1, num);
+		}
+}
+
+string toString (int number){
+		stringstream stream;
+        stream << number;
+        return stream.str() ;
+}
+
+int main(int argc, char*argv[]){
+	int x;
+	vector<int> arr;
+	int result;
+	string line, line2;
+	fstream file;
+	ifstream infile(argv[1]);
+	file.open(argv[2], ios::out);
+
+	getline(infile, line);
+	stringstream buffer(line);
+
+	while(buffer >> x){
+		arr.push_back(x);
+
+		if(buffer.peek() == ',') // >> std::ws)
+			buffer.ignore();
+	}
+	sort(arr.begin(), arr.end());
+	int y;
+	while(infile >> y)
+	{
+		counter = 0;
+		result = binarySearch(arr, 0, arr.size() - 1, y);
+		(result == -1) ? file<<toString(y)+" not found"<<endl : file<<toString(y)+" found at position "<< result+1 <<" after "<<counter<<" comparisons"<<endl;
+	}
+}
