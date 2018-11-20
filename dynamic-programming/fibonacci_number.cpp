703e199436016e4ce913c5faf644df795f5a3ede
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-11-21 05:06:21 +0530

    Optimized Version of Program

--- dynamic-programming/fibonacci_number.cpp
+++ dynamic-programming/fibonacci_number.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+
 int fibonacci_fast(int n) {
     // write your code here
-    int fin[n];
+    if ( n == 0 ) {
+        return 0;
+    }
+    int fin[3];
     fin[0]=0;
     fin[1]=1;
-    for (int i=2;i<=n;i++)
-    	fin[i]=fin[i-1]+fin[i-2];
-
-    return fin[n];
+    for (int i = 2; i <= n; i++) {
+    	fin[2] = fin[0] + fin[1];
+        fin[0] = fin[1];
+        fin[1] = fin[2];
+    }
+    return fin[1];
 }
+
 int main() {
     int n = 0;
     std::cin >> n;
@@ -16,3 +23,8 @@ int main() {
     std::cout << fibonacci_fast(n) << '\n';
     return 0;
 }
+/*
+Space Optimization for Fibonacci Number
+Space Complexity - O(1)
+Time Complexity - O(n)
+*/
