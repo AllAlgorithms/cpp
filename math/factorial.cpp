341e5275c8653b36428432bf3e1208644784c180
yashj0511 <72185189+yashj0511@users.noreply.github.com>
2020-10-01 12:35:30 +0530

    added endl and made it easy and clean to read and understandable

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -1,29 +1,32 @@
-//
+
 // C++ program to demonstrate Factorial Algorithm
-//
-// The All ▲lgorithms Project
-//
+// The All Algorithms Project
 // https://allalgorithms.com/math
 // https://github.com/allalgorithms/cpp
-//
 // Contributed by: Pablo Trinidad
 // Github: @pablotrinidad
-//
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int n) {
-    if (n <= 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
+int factorial(int n) 
+{
+    if (n <= 0)
+    {
+        return 1;    
+    } 
+    else
+    {
+       return n * factorial(n - 1);
     }
+    
 }
 
-int main() {
+int main()
+{
     int n;
-    cout << "Enter an integer n to compute its factorial: ";
+    cout << "Enter an integer n to compute its factorial: "<<endl;
     cin >> n;
     int r = factorial(n);
     cout << n << "! = " << r << endl;
