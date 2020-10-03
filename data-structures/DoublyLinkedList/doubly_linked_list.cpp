73ba459c30ba93f4e115e49267ea79c19c5e66fa
Rikhat <rikhat.akizhanov@nu.edu.kz>
2020-10-03 22:15:06 +0600

    Some minor C++ style code changes

--- data-structures/DoublyLinkedList/doubly_linked_list.cpp
+++ data-structures/DoublyLinkedList/doubly_linked_list.cpp
@@ -1,25 +1,27 @@
 #include"doubly_linked_list.h"
 
+using namespace std;
+
 template<class T>
 void DoublyLinkedList<T> :: Push_back(T val)
 {
 
-   Node<T>* new_node  = new Node<T>(val);
+    auto* new_node  = new Node<T>(move(val));
 
-   if(Tail != NULL) //or if(Size != 0)
-   {
-       Tail->next = new_node;
-       new_node->prev = Tail;
-       Tail = new_node;
+    if(Tail != nullptr) //or if(Size != 0)
+    {
+        Tail->next = new_node;
+        new_node->prev = Tail;
+        Tail = new_node;
 
-   }
-   else
-   {
-      Tail = new_node;
-      Head = new_node;
-   }
+    }
+    else
+    {
+        Tail = new_node;
+        Head = new_node;
+    }
 
-  Size_++;
+    Size_++;
 }
 
 
@@ -27,30 +29,29 @@ template<class T>
 void DoublyLinkedList<T> :: Pop_back()
 {
     //if list is empty
-   if(Tail == NULL) //or Head == NULL
-   {
-      cout<<"Can't pop back the DLS is empty"<<'\n';
-      return;
-   }
+    if(Tail == nullptr) //or Head == NULL
+    {
+        throw runtime_error("Can't pop back the DLS is empty");
+    }
 
-  if(Tail == Head) // if there's only one element in the DLS
-   {
-      delete Tail;
-      Tail = NULL;
-      Head = NULL;
+    if(Tail == Head) // if there's only one element in the DLS
+    {
+        delete Tail;
+        Tail = nullptr;
+        Head = nullptr;
 
-   }
-   else
-   {
-      Node<T>* previous_node = Tail->prev;
+    }
+    else
+    {
+        Node<T>* previous_node = Tail->prev;
 
-      delete Tail;
+        delete Tail;
 
-      Tail = previous_node;
-      Tail->next = NULL;
-   }
+        Tail = previous_node;
+        Tail->next = nullptr;
+    }
 
- Size_--;
+    Size_--;
 
 }
 
@@ -58,47 +59,46 @@ template <class T>
 void DoublyLinkedList<T> :: Push_front(T val)
 {
 
-  Node<T>* new_node = new Node<T>(val);
+    auto* new_node = new Node<T>(move(val));
 
-  new_node->next = Head;
-  if(Head != NULL)
-  {
-      Head->prev = new_node;
+    new_node->next = Head;
+    if(Head != nullptr)
+    {
+        Head->prev = new_node;
 
-  }
-  Head = new_node;
-  if(Tail == NULL)
-  {
-     Tail = Head;
-  }
+    }
+    Head = new_node;
+    if(Tail == nullptr)
+    {
+        Tail = Head;
+    }
 
- Size_++;
+    Size_++;
 
 }
 
 template<class T>
 void DoublyLinkedList<T> :: Pop_front()
 {
-  if(Head == NULL) //if dls is empty can't pop
-  {
-     cout<<"Can't pop front the DLS is empty"<<'\n';
-     return;
-  }
+    if(Head == nullptr) //if dls is empty can't pop
+    {
+        throw runtime_error("Can't pop front the DLS is empty");
+    }
 
-  Node<T>* next_node = Head->next;
-  delete Head;
-  Head = next_node;
+    Node<T>* next_node = Head->next;
+    delete Head;
+    Head = next_node;
 
-  if(Head == NULL) //if we popped the last element
-  {
-   Tail = NULL;
-  }
-  else
-  {
-     Head->prev = NULL;
-  }
+    if(Head == nullptr) //if we popped the last element
+    {
+        Tail = nullptr;
+    }
+    else
+    {
+        Head->prev = nullptr;
+    }
 
-Size_--;
+    Size_--;
 
 }
 
@@ -106,22 +106,22 @@ template<class T>
 void DoublyLinkedList<T> :: Add_before(Node<T>* node, T val)
 {
 
-     Node<T>* new_node = new Node<T>(val);
-     new_node->next = node;
-     new_node->prev = node->prev;
-     node->prev = new_node;
+    auto* new_node = new Node<T>(move(val));
+    new_node->next = node;
+    new_node->prev = node->prev;
+    node->prev = new_node;
 
-     if(new_node->prev != NULL)
-     {
-         new_node->prev->next = new_node;
-     }
+    if(new_node->prev != nullptr)
+    {
+        new_node->prev->next = new_node;
+    }
 
-     if(Head == node)
-     {
-       Head = new_node;
+    if(Head == node)
+    {
+        Head = new_node;
 
-     }
-  Size_++;
+    }
+    Size_++;
 }
 
 
@@ -130,44 +130,44 @@ template <class T>
 void DoublyLinkedList<T> :: Add_after(Node<T>* node,T val)
 {
 
-   Node<T>* new_node = new Node<T>(val);
-   new_node->prev = node;
-   new_node->next = node->next;
-   node->next = new_node;
+    auto* new_node = new Node<T>(move(val));
+    new_node->prev = node;
+    new_node->next = node->next;
+    node->next = new_node;
 
 
-   if(new_node->next != NULL)
-   {
-       new_node->next->prev = new_node;
+    if(new_node->next != nullptr)
+    {
+        new_node->next->prev = new_node;
 
-   }
+    }
 
-   if(Tail == node)
-   {
-      Tail = new_node;
-   }
+    if(Tail == node)
+    {
+        Tail = new_node;
+    }
 
-Size_++;
+    Size_++;
 
 }
 
 template <class T>
-void DoublyLinkedList<T> :: Display()
+void DoublyLinkedList<T> :: Display() const
 {
 
-     if(Size_ == 0)
-     {
-       cout<<"Linked List is empty";
-     }
-     else
-     {
-        for(Node<T>* tmp_ptr = Head;tmp_ptr!= NULL; tmp_ptr= tmp_ptr->next)
+    if(Size_ == 0)
+    {
+        cout<<"Linked List is empty";
+    }
+    else
+    {
+        for(Node<T>* tmp_ptr = Head;tmp_ptr!= nullptr; tmp_ptr= tmp_ptr->next)
         {
-             cout<<tmp_ptr->data<<" ";
+            cout<<tmp_ptr->data<<" ";
 
         }
-     }
-cout<<'\n';
+    }
+    cout<<'\n';
 }
 
 
@@ -179,91 +179,90 @@ void DoublyLinkedList<T> :: Clear()
     Node<T>* tmp = Head;
     if(Size_ == 0 )
     {
-         cout<<" all cleared linked list is empty"<<'\n';
-         return;
+        throw runtime_error(" all cleared linked list is empty");
     }
 
-    while(Head != NULL)
+    while(Head != nullptr)
     {
 
-         Head = Head->next;
-         delete tmp;
-         tmp = Head;
+        Head = Head->next;
+        delete tmp;
+        tmp = Head;
 
     }
-  cout<<" all cleared linked list is empty"<<'\n';
-Tail = NULL;
-Size_ = 0;
+    cout<<" all cleared linked list is empty"<<'\n';
+    Tail = nullptr;
+    Size_ = 0;
 }
 template <class T>
 void DoublyLinkedList<T> :: Insert_at(T val ,int position)
 {
-  if(position >Size_ || position <= 0)
+    if(position >Size_ || position <= 0)
     {
-       cout<<"invalid position choose a position between 1 and size "<<'\n';
-       cout<<"size is: "<<Size_<<'\n';
+        cout<<"invalid position choose a position between 1 and size "<<'\n';
+        cout<<"size is: "<<Size_<<'\n';
 
     }
- else
- {
-       Node<T>* tmp = Head;
-      //get a pointer of that position that position
-      for(int i =1 ; i<=position-1 ; i++,tmp = tmp->next);
-      Add_before(tmp,val);
- }
+    else
+    {
+        Node<T>* tmp = Head;
+        //get a pointer of that position that position
+        for(int i =1 ; i<=position-1 ; i++,tmp = tmp->next);
+        Add_before(tmp,val);
+    }
 
 
- }
+}
 
 template<class T>
 void DoublyLinkedList<T> :: Delete_at(int position)
 {
 
 
-  if(Size_ ==0)
-  {
-      cout<<"Can't delete DLS is empty "<<'\n';
-      return;
-  }
-
-  if(position >Size_ || position < 0)
+    if(Size_ ==0)
     {
-       cout<<"invalid position choose a position between 1 and size "<<'\n';
-       cout<<"size is: "<<Size_<<'\n';
-       return;
+        cout<<"Can't delete DLS is empty "<<'\n';
+        return;
+    }
+
+    if(position >Size_ || position < 0)
+    {
+        cout<<"invalid position choose a position between 1 and size "<<'\n';
+        cout<<"size is: "<<Size_<<'\n';
+        return;
 
     }
 
 
-   //general deletion of nodes in DLS
-      Node<T>* tmp = Head;
+    //general deletion of nodes in DLS
+    Node<T>* tmp = Head;
 
-      for(int i = 1; i <= position-1; i++,tmp = tmp->next);
+    for(int i = 1; i <= position-1; i++,tmp = tmp->next);
 
-       if(tmp->next != NULL)
-       {
-           tmp->next->prev = tmp->prev;
-       }
+    if(tmp->next != nullptr)
+    {
+        tmp->next->prev = tmp->prev;
+    }
 
-       if(tmp->prev != NULL)
-       {
-            tmp->prev->next = tmp->next;
+    if(tmp->prev != nullptr)
+    {
+        tmp->prev->next = tmp->next;
 
-       }
+    }
 
-       if(Head == tmp)
-       {
-         Head = tmp->next;
+    if(Head == tmp)
+    {
+        Head = tmp->next;
 
-       }
+    }
 
-       if(Tail == tmp)
-       {
-          Tail = Tail->prev;
-       }
+    if(Tail == tmp)
+    {
+        Tail = Tail->prev;
+    }
 
-delete tmp;
-Size_--;
+    delete tmp;
+    Size_--;
 
 
 }
