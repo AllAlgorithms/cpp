1f3b65cf7fe1474c33a2def9178fc501ad2613a3
Bilal Zafar <bilalzaafar@gmail.com>
2020-10-23 06:51:07 +0500

    Update prime.cpp
    We can find the prime number by running only to the half of the number entered. In this way the running time can be decreased. Also we can use bool variable to keep track of prime number rather than integer which is more memory efficient

--- algorithms/math/prime.cpp
+++ algorithms/math/prime.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
-int main()
-{int n,flag;
-cin>>n;
-//to check whether a number is prime or not
-for(int i=2;i<n;i++){
- if(n%i==0)
- flag++;}
- if(flag>0)
- cout<<"number is not prime"<<endl;
- else cout<<"number is prime"<<endl;
+int main(){
+ int n;
+ cin>>n;
+ //to check whether a number is prime or not
+ bool isPrime=true;
+ for(int i = 2 ;i <= n/2; i++){
+     if( n%i == 0){
+     isPrime=false;
+     break;
+     }
+ }
+ if(isPrime) cout<<"Number is Prime"<<endl;
+ else cout<<"Number is not prime"<<endl;
  return 0;
  }
