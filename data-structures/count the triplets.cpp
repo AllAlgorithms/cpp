8c421cb50fbbc3d53f948356b363b025757842db
Divyanshu Bisht <57855321+divyanshubisht@users.noreply.github.com>
2021-02-09 15:19:15 +0530

    Add files via upload
    counting triplets with best time complexity possible

---
+++ data-structures/count the triplets.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+    for(i=0;i<n-1;i++)
+    {   k=i+2;
+        for(j=i+1;j<n;j++)
+        {
+
+            if(arr[i]+arr[j]==arr[k++]
+            else
+            {
+                if(arr[i]+arr[j]==arr[k])
+            }
+        }
+    }
+}
